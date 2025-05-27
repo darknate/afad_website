bf693e2d0876a8af61d17848baad081720fa59da
Alex Fadeev <darknate@users.noreply.github.com>
2025-05-27 10:59:49 +0200

    remove hidden folder

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/afadeev/GitHub/afad_website/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/afadeev/GitHub/afad_website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/afadeev/GitHub/afad_website/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/afadeev/GitHub/afad_website/src/css/custom.css"),
-  require("/Users/afadeev/GitHub/afad_website/node_modules/@docusaurus/plugin-google-gtag/lib/gtag"),
-];
--- .docusaurus/registry.js
+++
@@ -1,7 +0,0 @@
-export default {
-  "1f391b9e": [() => import(/* webpackChunkName: "1f391b9e" */ "@theme/MDXPage"), "@theme/MDXPage", require.resolveWeak("@theme/MDXPage")],
-  "393be207": [() => import(/* webpackChunkName: "393be207" */ "@site/src/pages/markdown-page.md"), "@site/src/pages/markdown-page.md", require.resolveWeak("@site/src/pages/markdown-page.md")],
-  "5e9f5e1a": [() => import(/* webpackChunkName: "5e9f5e1a" */ "@generated/docusaurus.config"), "@generated/docusaurus.config", require.resolveWeak("@generated/docusaurus.config")],
-  "7792a21f": [() => import(/* webpackChunkName: "7792a21f" */ "@site/src/pages/about.js"), "@site/src/pages/about.js", require.resolveWeak("@site/src/pages/about.js")],
-  "a7456010": [() => import(/* webpackChunkName: "a7456010" */ "@generated/docusaurus-plugin-content-pages/default/__plugin.json"), "@generated/docusaurus-plugin-content-pages/default/__plugin.json", require.resolveWeak("@generated/docusaurus-plugin-content-pages/default/__plugin.json")],
-  "c4f5d8e4": [() => import(/* webpackChunkName: "c4f5d8e4" */ "@site/src/pages/index.js"), "@site/src/pages/index.js", require.resolveWeak("@site/src/pages/index.js")],};
